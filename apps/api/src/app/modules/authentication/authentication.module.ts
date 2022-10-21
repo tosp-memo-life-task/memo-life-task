d1a360ddc5e2f56ae2c99d9cfa0b2004db0b9dad
Benjámin György Héra <o1c2m9@inf.elte.hu>
2022-10-21 17:45:27 +0200

    🔧 📝 Added jwt auth guard

--- apps/api/src/app/modules/authentication/authentication.module.ts
+++ apps/api/src/app/modules/authentication/authentication.module.ts
@@ -5,8 +5,10 @@ import { AuthenticationController } from './authentication.controller';
 import { SignUpService } from './services/sign-up/sign-up.service';
 import { SignInService } from './services/sign-in/sign-in.service';
 
+import { JwtStrategy } from './strategies/jwt.strategy';
+
 @Module({
   controllers: [AuthenticationController],
-  providers: [SignUpService, SignInService]
+  providers: [JwtStrategy, SignUpService, SignInService]
 })
 export class AuthenticationModule {}
---
+++ apps/api/src/app/modules/authentication/guards/jwt.guard.ts
@@ -0,0 +1,26 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
+  constructor() {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
