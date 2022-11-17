e070c4706609d90a0e46c1a05d0f8fb4b32f0113
Benjámin György Héra <o1c2m9@inf.elte.hu>
2022-11-17 12:46:40 +0100

    🗃️ 🏷️ Created every entity.

--- apps/api/src/app/database/entities/user.entity.ts
+++ apps/api/src/app/database/entities/user.entity.ts
@@ -1,4 +1,16 @@
-import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm';
+
+import { Task } from './task.entity';
+import { Workspace } from './workspace.entity';
 
 @Entity()
 export class User {
@@ -8,4 +20,28 @@ export class User {
   @Column({ name: 'email', nullable: false })
   @Index({ unique: true })
   email: string;
+
+  @Column({ name: 'password', nullable: false })
+  password: string;
+
+  @Column({ name: 'salt', nullable: false })
+  salt: string;
+
+  @Column({ name: 'name_first', nullable: false })
+  nameFirst: string;
+
+  @Column({ name: 'name_last', nullable: false })
+  nameLast: string;
+
+  @CreateDateColumn({ name: 'registered_at' })
+  registeredAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToMany(() => Task, (task) => task.assignee)
+  tasks: Task[];
+
+  @ManyToMany(() => Workspace, (workspace) => workspace.users)
+  workspaces: Workspace[];
 }
