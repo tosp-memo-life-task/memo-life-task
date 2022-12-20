58a0c64af33436ac07ccc5a79e208a08b5e7c83c
Benjámin György Héra <o1c2m9@inf.elte.hu>
2022-12-20 19:59:31 +0100

    🚧 wip

--- apps/api/src/app/modules/invitation/services/revoke-invitation/revoke-invitation.service.ts
+++ apps/api/src/app/modules/invitation/services/revoke-invitation/revoke-invitation.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
+import { InvitationRepository } from '../../../../database/repositories/invitation.repository';
+
+import { ValidatedUserModel } from '../../../../common/models/validated-user.model';
+
+import { CommonDatabaseErrorException } from '../../../../common/exceptions/common-database-error.exception';
+import { InvitationNotFoundException } from '../../../../common/exceptions/invitation-not-found.exception';
+import { WorkspaceUnauthroziedException } from '../../../../common/exceptions/workspace-unauthorized.exception';
+
 @Injectable()
-export class RevokeInvitationService {}
+export class RevokeInvitationService {
+  constructor(private readonly invitationRepository: InvitationRepository) {}
+
+  async revokeInvitation(
+    params: any,
+    validatedUser: ValidatedUserModel
+  ): Promise<void> {
+    const invitation = await this.invitationRepository
+      .findOneOrFail({
+        relations: { sender: true },
+        where: { id: params.id }
+      })
+      .catch(() => {
+        throw new InvitationNotFoundException();
+      });
+
+    if (invitation.sender.id !== validatedUser.id)
+      throw new WorkspaceUnauthroziedException();
+
+    await this.invitationRepository.remove(invitation).catch((err) => {
+      throw new CommonDatabaseErrorException(err);
+    });
+  }
+}
--- libs/dtos/src/lib/invitation/send-invitation/send-invitation.request.body.ts
+++ libs/dtos/src/lib/invitation/send-invitation/send-invitation.request.body.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsPositive } from 'class-validator';
+import { IsEmail, IsPositive } from 'class-validator';
 
 import { ISendInvitationRequestBody } from '@memo-life-task/interfaces';
 
 export class SendInvitationRequestBody implements ISendInvitationRequestBody {
   @ApiProperty()
-  @IsPositive()
-  receiverId: number;
+  @IsEmail()
+  email: string;
 
   @ApiProperty()
   @IsPositive()
