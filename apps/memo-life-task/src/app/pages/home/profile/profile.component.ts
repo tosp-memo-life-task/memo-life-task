7286bf74a51dc1e247fb1676e4fe6c4a87138098
Barnabás Pintér <smsz00@inf.elte.hu>
2022-12-20 16:34:01 +0100

    most of the elements are plaed on screen

--- apps/memo-life-task/src/app/pages/home/profile/profile.component.ts
+++ apps/memo-life-task/src/app/pages/home/profile/profile.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tosp-memo-life-task-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  form: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({});
+  }
+  async onSubmit(): Promise<void> {
+    console.log('todo');
+  }
+}
